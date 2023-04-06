b8f58067de9235fb7a244eeb4a4aaf9cbf681a79
akramov_developer <bahrom.akramov98@gmail.com>
2023-04-07 01:29:27 +0500

    complited

--- Manager.Application/Handlers/CategoryHandler.cs
+++ Manager.Application/Handlers/CategoryHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Manager.Application.Interfaces;
+using Manager.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace Manager.Application.Handlers
 {
-    internal class CategoryHandler
+    public class CategoryHandler : IRepository<Category>
     {
+        public Task<bool> DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Category>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> InsertAsync(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(Category entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Manager.Infrastructure/Repositories/Models/CategoryRepo.cs
+++ Manager.Infrastructure/Repositories/Models/CategoryRepo.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using Manager.Domain.Models;
+using Manager.Infrastructure.Connection;
+using Manager.Infrastructure.Repositories.Interfaces;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,76 @@ using System.Threading.Tasks;
 
 namespace Manager.Infrastructure.Repositories.Models
 {
-    internal class CategoryRepo
+    public class CategoryRepo : IRepository<Category>
     {
+
+        private readonly string _connection;
+        public CategoryRepo()
+        {
+            _connection = GetConnection.Connection();
+        }
+        public async Task<bool> DeleteByIdAsync(int categoryId)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                await conn.OpenAsync();
+                string cmdText = @"delete from categories where category_id=@id;";
+                if (await conn.ExecuteAsync(cmdText, new { id = categoryId }) > 0) return true;
+                else return false;
+            }
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                List<Category> categories = new List<Category>();
+                await conn.OpenAsync();
+                string cmdText = @"select 
+                                        category_id as CategoryId,
+                                        category_name as CategoryName
+                                   from categories;";
+                categories = (await conn.QueryAsync<Category>(cmdText)).ToList();
+                return categories;
+            }
+        }
+
+        public async Task<Category> GetByIdAsync(int id)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                Category categorie = new Category();
+                await conn.OpenAsync();
+                string cmdText = @"select 
+                                        category_id as CategoryId,
+                                        category_name as CategoryName
+                                   from categories;";
+                categorie = await conn.QueryFirstOrDefaultAsync<Category>(cmdText);
+                return categorie;
+            }
+        }
+
+        public async Task<bool> InsertAsync(Category category)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                await conn.OpenAsync();
+                string cmdText = @"insert into categories(category_name)
+                                   values(@CategoryName);";
+                if (await conn.ExecuteAsync(cmdText, category) > 0) return true;
+                else return false;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Category category)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                await conn.OpenAsync();
+                string cmdText = @"update categories set category_name=@CategoryName;";
+                if (await conn.ExecuteAsync(cmdText, category) > 0) return true;
+                else return false;
+            }
+        }
     }
 }
