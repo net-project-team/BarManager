501104fbd97f73ee359618c5feede83750b9ac43
GetSomeHelp <otajonovzafar2003@gmail.com>
2023-04-07 16:29:47 +0300

    Finished 1 - IWaiterRepository, 2 - IwaiterHandler and 3 -  WaiterHandler

--- Manager.Application/Handlers/WaiterHandler.cs
+++ Manager.Application/Handlers/WaiterHandler.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Manager.Application.Handler.Interfaces;
+using Manager.Application.Repository.Interfaces;
+using Manager.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace Manager.Application.Handlers
 {
-    public class WaiterHandler
+    public class WaiterHandler : IWaiterHandler
     {
+        private readonly IWaiterRepository _repository;
+
+        public WaiterHandler(IWaiterRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> DeleteWaiterByIdAsync(int id)
+        {
+            return await _repository.DeleteByIdAsync(id);
+        }
+
+        public async Task<List<Waiter>> GetAllWaitersAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<Waiter> GetByIdWaiterAsync(int waiterId)
+        {
+            return await _repository.GetByIdAsync(waiterId);
+        }
+
+        public async Task<bool> InsertWaiterAsync(Waiter waiter)
+        {
+            return await _repository.InsertAsync(waiter);
+        }
+
+        public async Task<bool> UpdateWaiterAsync(Waiter waiter)
+        {
+            return await _repository.UpdateAsync(waiter);
+        }
     }
 }
