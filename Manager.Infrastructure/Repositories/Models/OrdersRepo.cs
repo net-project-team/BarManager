b27e735e69d90d0cd09daa147da53839206051ce
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-04-07 08:38:59 +0500

    added orders db crud

---
+++ Manager.Infrastructure/Repositories/Models/OrdersRepo.cs
@@ -0,0 +1,96 @@
+﻿using Dapper;
+using Manager.Domain.Models;
+using Manager.Infrastructure.Connection;
+using Manager.Infrastructure.Repositories.Interfaces;
+using Npgsql;
+
+namespace Manager.Infrastructure.Repositories.Models
+{
+    public class OrdersRepo : IRepository<Order>
+    {
+        private readonly string _connection;
+        public OrdersRepo()
+        {
+            _connection = GetConnection.Connection();
+        }
+
+        public async Task<bool> DeleteByIdAsync(int orderId)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                await conn.OpenAsync();
+                string cmdText = @"delete from orders where order_id = @id";
+                if (await conn.ExecuteAsync(cmdText, new { id = orderId }) > 0) return true;
+                else return false;
+
+            }
+        }
+
+        public async Task<List<Order>> GetAllAsync()
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                List<Order> orders = new List<Order>();
+                await conn.OpenAsync();
+                string cmdText = @"select order_id as OrderId,
+                                   product_id as ProductId,
+                                   waiter_id as WaiterId,
+                                   order_table as OrderTable,
+                                   order_date as OrderDate,
+                                   is_completed as IsCompleted
+                                   from orders;";
+                orders = (await conn.QueryAsync<Order>(cmdText)).ToList();
+                return orders;
+            }
+        }
+
+        public async Task<Order> GetByIdAsync(int orderId)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                Order orders = new Order();
+                await conn.OpenAsync();
+                string cmdText = @"select order_id as OrderId,
+                                   product_id as ProductId,
+                                   waiter_id as WaiterId,
+                                   order_table as OrderTable,
+                                   order_date as OrderDate,
+                                   is_completed as IsCompleted
+                                   from orders where order_id = @id;";
+                orders = await conn.QueryFirstOrDefaultAsync<Order>(cmdText, new { id = orderId });
+                return orders;
+            }
+        }
+
+        public async Task<bool> InsertAsync(Order orders)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                await conn.OpenAsync();
+                string cmdText = @"insert into orders(order_id, product_id,
+                                   waiter_id, order_table, order_date, is_completed)
+                                    values (@OrderId, @ProductId, @WaiterId, @OrderTable, @OrderDate,
+                                    @IsCompleted);";
+                if (await conn.ExecuteAsync(cmdText, orders) > 0) return true;
+                else return false;
+            }
+        }
+
+        public async Task<bool> UpdateAsync(Order orders)
+        {
+            using(NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                await conn.OpenAsync();
+                string cmdText = @"udate orders set 
+                                   product_id as ProductId,
+                                   waiter_id = @WaiterId,
+                                   order_table = @OrderTable,
+                                   order_date = @OrderDate,
+                                   is_completed = @IsCompleted
+                                   where order_id = @OrderId,";
+                if(await conn.ExecuteAsync(cmdText, orders)>0) return true;
+                else return false;
+            }
+        }
+    }
+}
