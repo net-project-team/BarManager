9c331a9033ada4ac36c34b001abefa08e26c3c93
Elyorbek Tojidinov <tojidinovbekelyorbek@gmail.com>
2023-04-08 18:02:19 +0500

    Fixed main

--- Manager.Presentation/Program.cs
+++ Manager.Presentation/Program.cs
@@ -17,22 +17,48 @@ namespace Manager.Presentation
             //IWaiterHandler waiterHandler = new WaiterHandler(waiterRepo);
             //Console.WriteLine("AAAAuhgh");
 
-            //IProductRepository productRepository = new ProductsRepo();
+            IWaiterRepository waiterRepo = new WaiterRepo();
+            IWaiterHandler waiterHandler = new WaiterHandler(waiterRepo);
+            Console.WriteLine("AAAAuhgh");
+
+            IProductRepository productRepository = new ProductsRepo();
 
             var a = waiterHandler.GetByIdWaiterAsync(1).Result;
             Console.WriteLine(a.WaiterName);
+            Console.WriteLine("aaa");
+
+
+
+
+            //IProductRepository productRepository = new ProductsRepo();
+
+            //var a = waiterHandler.GetByIdWaiterAsync(1).Result;
+            //Console.WriteLine(a.WaiterName);
 
             
             RunOrders();
             Console.ReadKey();
         }
 
+        public async static void RunOrders()
+        {
+            IOrdersRepository orRepo = new OrdersRepo();
+            IOrdersHandler orHand = new OrdersHandler(orRepo);
+            // Order or =  await orHand.GetByIdOrdersAsync(1);
+            //Console.WriteLine(or);
+            List<Order> orList = await orHand.GetAllOrdersAsync();
 
 
             foreach (Order or in orList)
             {
                 Console.WriteLine(or);
             }
+
+            //IRepository<Category> CategoryRepo = new CategoryRepo();
+            //IRepository<Category> CategoryHandler = new CategoryHandler(CategoryRepo);
+
+
+
         }
     }
 }
