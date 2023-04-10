e6dc818bfbd2345a89e0449f5392a3c88de7bca1
akramov_developer <bahrom.akramov98@gmail.com>
2023-04-10 13:33:09 +0500

    some changes

--- Manager.Infrastructure/Connection/GetConnection.cs
+++ Manager.Infrastructure/Connection/GetConnection.cs
@@ -9,12 +9,13 @@ namespace Manager.Infrastructure.Connection
         private static readonly string _path3 = @"D:\BarManager\Manager.Infrastructure\Connection\appSettings.json";
         private static readonly string _path2 = @"C:\Users\HP\Source\Repos\BarManager\Manager.Infrastructure\Connection\appSettings.json";
         private static readonly string _elyorbek = @"D:\C# darslar\Bar_Manager\bar_manager\Manager.Infrastructure\Connection\appSettings.json";
+        private static readonly string _bahrom = @"C:\Users\Akramov_Developer\Desktop\Proj\bar_manager\Manager.Infrastructure\Connection\appSettings.json";
         //D:\C# darslar\Bar_Manager\bar_manager\Manager.Infrastructure\Connection\appSettings.json
         public static string Connection()
         {
             var builder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile(_path3, true, true);
+            .AddJsonFile(_bahrom, true, true);
             
             IConfiguration configuration = builder.Build();
             return  configuration.GetConnectionString("MyConnectionString");
