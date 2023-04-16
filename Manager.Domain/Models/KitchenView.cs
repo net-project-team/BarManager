293e373acd93ec744bbd27efdd54e29bdec52e94
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-04-16 20:03:35 +0500

    big changes, added kitchen page, some fix product_cart page

---
+++ Manager.Domain/Models/KitchenView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Domain.Models
+{
+    public class KitchenView
+    {
+        public int OrderId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
---
+++ Manager.Infrastructure/Repositories/Views/GetKitchenOrder.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Manager.Domain.Models;
+using Manager.Infrastructure.Connection;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Infrastructure.Repositories.Views
+{
+    public class GetKitchenOrder
+    {
+        private readonly string _connection;
+        public GetKitchenOrder()
+        {
+            _connection = GetConnection.Connection();
+        }
+        public async Task<List<KitchenView>> GetKichenViews()
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+                List<KitchenView> kitchenViews = new List<KitchenView>();
+                await conn.OpenAsync();
+                string cmdText = @"select order_id as OrderId,
+                                   product_name as ProductName,
+                                   count as ProductCount
+                                   from for_trigger_product_order_view";
+                kitchenViews = (await conn.QueryAsync<KitchenView>(cmdText)).ToList();
+                return kitchenViews;
+            }
+
+        }
+        public async Task<bool> DeleteKitchenOrder(int id)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connection))
+            {
+
+                await conn.OpenAsync();
+                string cmdText = @"delete from triggered_product_cart 
+                                   where order_id = @orderId";
+                if( await conn.ExecuteAsync(cmdText, new {orderId = id})>0)return true;
+                else return false;
+                    
+
+            }
+        }
+        
+
+    }
+}
